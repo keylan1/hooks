481826c95febe0eda7bc8c4d4f7585c953d22a72
Sarah.Keller <sarah29.84@gmail.com>
2024-03-27 20:04:06 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
 import './App.css';
 
+const reducer = (state, action) => {
+  if (action.type === 'buy_ingredients') return (money: state.money - 10);
+  if (action.type === 'sell_a_meal') return (money: state.money + 10);
+}
 function App() {
+
+  const initialState = {money: 100};
+  const {state, dispatch} = useReducer(reducer, initialState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wallet: {state.money}</h1>
+      <div>
+        <button onClick={() => dispatch({type: "buy_ingredients"})}>Shopping for ingredients</button>
+        <button onClick={() => dispatch({type: "sell_a_meal"})}>Served meal to customer</button>
+      </div>
     </div>
   );
 }
