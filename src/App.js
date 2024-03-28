6a58dd33f033209a1cab26be3957180b4c2b41c0
Sarah.Keller <sarah29.84@gmail.com>
2024-03-28 18:18:23 +0100

    Added action type

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import { useReducer } from 'react';
 import './App.css';
 
 const reducer = (state, action) => {
-  if (action.type === 'buy_ingredients') return (money: state.money - 10);
-  if (action.type === 'sell_a_meal') return (money: state.money + 10);
-}
-function App() {
+  if (action.type === 'buy_ingredients') return { money: state.money - 10 };
+  if (action.type === 'sell_a_meal') return { money: state.money + 10 };
+  if (action.type === 'special_event') return { money: state.money + 1000 };
+  return state;
+};
 
-  const initialState = {money: 100};
-  const {state, dispatch} = useReducer(reducer, initialState);
+function App() {
+  const initialState = { money: 100 };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div className="App">
       <h1>Wallet: {state.money}</h1>
       <div>
-        <button onClick={() => dispatch({type: "buy_ingredients"})}>Shopping for ingredients</button>
-        <button onClick={() => dispatch({type: "sell_a_meal"})}>Served meal to customer</button>
+        <button onClick={() => dispatch({ type: 'buy_ingredients' })}>
+          Shopping for ingredients
+        </button>
+        <button onClick={() => dispatch({ type: 'sell_a_meal' })}>
+          Served meal to customer
+        </button>
+        <button onClick={() => dispatch({ type: 'special_event' })}>
+          Special event
+        </button>
       </div>
     </div>
   );
